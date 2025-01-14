f825ab8be07e33d6f9965b3aa3d792118b70865a
Celeste-clt <celeste.colletti@icloud.com>
2025-01-14 11:52:50 +0100

    change js

--- a-propos.js
+++ a-propos.js
@@ -1,11 +1,14 @@
+/* Button page à porpos*/
+
+
 document.getElementById('cvButton').addEventListener('click', function () {
     const image = 'Céleste.png';
     window.open(image);
 });
 
 document.getElementById('contactez').addEventListener('click', function () {
-    // URL de la page HTML de destination
-    var pageUrl = 'contact.html';  // Remplace par le nom de la page de destination
-    // Redirection vers la nouvelle page
+
+    var pageUrl = 'contact.html';  
+   
     window.location.href = pageUrl;
 });
