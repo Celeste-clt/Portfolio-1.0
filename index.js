4fea5a181f83431539fa662d10a1eda3c1f2df00
Celeste-clt <celeste.colletti@icloud.com>
2025-01-14 22:07:07 +0100

    js swip

--- index.js
+++ index.js
@@ -63,39 +63,35 @@ document.body.appendChild(buttonContainer);
 
 /* swip sur quiz.html*/
 
-// Créer le conteneur pour les boutons
-const buttonContainer = document.createElement('div');
-buttonContainer.classList.add('button-container');
 
-// Créer le premier bouton (Projets précédent)
 const buttonPrev = document.createElement('button');
 buttonPrev.classList.add('swip-projets');
 
-// Créer le lien pour "Projets précédent"
+
 const linkPrev = document.createElement('a');
 linkPrev.classList.add('gal');
 linkPrev.href = 'italie.html';
 linkPrev.textContent = 'Projets précédent';
 
-// Ajouter le lien au premier bouton
+
 buttonPrev.appendChild(linkPrev);
 
-// Créer le deuxième bouton (Projets suivant)
+
 const buttonNext = document.createElement('button');
 buttonNext.classList.add('swip-projets');
 
-// Créer le lien pour "Projets suivant"
+
 const linkNext = document.createElement('a');
 linkNext.classList.add('gal');
 linkNext.href = 'cache.html';
 linkNext.textContent = 'Projets suivant';
 
-// Ajouter le lien au deuxième bouton
+
 buttonNext.appendChild(linkNext);
 
-// Ajouter les boutons au conteneur
+
 buttonContainer.appendChild(buttonPrev);
 buttonContainer.appendChild(buttonNext);
 
-// Ajouter le conteneur au corps du document (ou autre conteneur)
+
 document.body.appendChild(buttonContainer);
