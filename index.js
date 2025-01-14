05b202f2841827575948ee5c8003f1f27fa9ba0f
Celeste-clt <celeste.colletti@icloud.com>
2025-01-14 22:02:35 +0100

    projets

--- index.js
+++ index.js
@@ -37,3 +37,27 @@ window.onload = () => {
   console.log(swiper);  
 };
 
+/* swip projets */
+
+// Créer la structure du bouton et du lien
+const buttonContainer = document.createElement('div');
+buttonContainer.classList.add('button-container');
+
+// Créer le bouton
+const button = document.createElement('button');
+button.classList.add('swip-projets');
+
+// Créer le lien
+const link = document.createElement('a');
+link.classList.add('gal');
+link.href = 'quiz.html';
+link.textContent = 'Projets suivant';
+
+// Ajouter le lien au bouton
+button.appendChild(link);
+
+// Ajouter le bouton au conteneur
+buttonContainer.appendChild(button);
+
+// Ajouter le conteneur à l'élément body (ou à un autre conteneur spécifique)
+document.body.appendChild(buttonContainer);
